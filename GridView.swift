862ba9c520e07e4e9c4671ff1f58972a7bacd749
Mihail <153065029+zuravlevmihail@users.noreply.github.com>
2024-02-02 05:19:23 +0100

    Update GridView.swift

--- GridView.swift
+++ GridView.swift
@@ -17,3 +17,21 @@ struct GridView: View {
             HStack {
                 ForEach(0...xLines, id: \.self) {
                     createYLine(index: $0)
+                }
+            }
+        }
+        
+    }
+    
+    @ViewBuilder
+    private func createYLine(index: Int) -> some View {
+        if index == 0 {
+            rectangle
+                .frame(width: lineWidth)
+        } else {
+            HStack(spacing: 0) {
+                Spacer()
+                    .frame(minWidth: 0)
+                rectangle
+                    .frame(width: lineWidth)
+            }
